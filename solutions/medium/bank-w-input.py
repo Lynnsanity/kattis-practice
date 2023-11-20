76f8c63825da522520b91fe3c1631c93c6453627
Lynnsanity <lynnellesaavedra8@gmail.com>
2023-11-19 18:20:57 -0600

    fixed to new answer

--- solutions/medium/bank-w-input.py
+++ solutions/medium/bank-w-input.py
@@ -1,6 +1,9 @@
-space = ' '
+# start the total cash at 0 because money will be counted as we go
 total_cash = 0
-customers = []
+# this is for the amount of money we can get, but it needs checks afterward
+potential_cash = []
+# customer dict containing leave times and their associating cash amounts
+customer_dict = {}
 
 with open('input.txt') as file:
     # make the entire first line a variable,
@@ -8,26 +11,34 @@ with open('input.txt') as file:
     first_line = file.readline().split()
     people_in_queue = int(first_line[0])
     time_remaining = int(first_line[1])
-    print("Number of people in queue:", people_in_queue, "Time before closing:", time_remaining)
-    # the first readline already upped the file line counter to 1,
-    # so the values in here skip the first line
-    for line in file:
-        # split whatever line we're on (by default it'll split spaces)
-        cash_and_time = line.split()
-        # the first number is cash
-        customer_cash = int(cash_and_time[0])
-        # second number is minutes representing when people are gonna leave
-        customer_leave_time = int(cash_and_time[1])
-        # make a tuple so both customer cash and leave time can correspond with each other and attach it onto the list of customers
-        customers.append((customer_cash, customer_leave_time))
 
-# reverse the order of the customer list so that its starts with greater cash amounts
-customers.sort(reverse=True)
+# the first readline already upped the file line counter to 1,
+# so the values in here skip the first line
+for line in file:
+    # the whole input line is both the cash and time
+    cash_and_time = line.split()
+    # the customer cash is the first part of line
+    customer_cash = int(cash_and_time[0])
+    # the customers amount of time in which they'll leave is the second part of line
+    customer_leave_time = int(cash_and_time[1])
 
-# make sure we can serve the customer, and when we do decrease by time by 1 min.
-for customer_cash, customer_leave_time in customers:
-    if time_remaining >= customer_leave_time + 1:
-        total_cash += customer_cash
-        time_remaining -= 1
+    # if the customer leave time repeats, the customer cash will be appended to that time.
+    customer_dict.setdefault(customer_leave_time, []).append(customer_cash)
 
+# a reverse iterator, considers customers leaving closer to closing time
+for time in range(time_remaining)[::-1]:
+    # are there customers leaving at the current time? if so, go into the if statement
+    if time in customer_dict.keys():
+        # if there's more than one person leaving at the same time, it goes over each cash amount and puts it in a list called potential_cash
+        for cash in customer_dict[time]:
+            potential_cash.append(cash)
+        # if there was a potential cash found then go into if statement
+        if potential_cash:
+            # add the max potential cash to total cash
+            total_cash += max(potential_cash)
+            # reset the potential_cash list so the next iteration can be fresh
+            potential_cash.remove(max(potential_cash))
+
+# total money we can get :D
 print(total_cash)
+
